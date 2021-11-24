6a0e6b958bb3f6c29dd2ae1aaea0309a69976399
Maryll Castelino <maryllcastelino@gmail.com>
2021-11-24 12:20:55 +0530

    added error message

--- lab7/subsetSum.py
+++ lab7/subsetSum.py
@@ -1,7 +1,10 @@
 #!/bin/python
 
 def subset(sum, i, count):
+    # print(count)
     if sum == d:
+        global flag
+        flag = 1
         display(count)
         return
 
@@ -9,13 +12,13 @@ def subset(sum, i, count):
         return
 
     else:
-        # print(i)
         set[count] = array[i]
 
         count += 1
         subset(sum + array[i], i+1, count)
         count -= 1
         subset(sum, i+1, count)
+    
 
 
 def display(c):
@@ -30,9 +33,11 @@ def display(c):
 
 if __name__ == "__main__":
     set = [0]*10
-
+    flag = 0
     # array = [1, 2, 5, 6, 8]
     array = list(map(int,input("\nEnter elements of array : ").split()))
     d = int(input("Enter the value of d: "))
     count = 0
     subset(0, 0, 0)
+    if flag == 0:
+        print("No subsets were found")
